ddb1dfcffa0fdc8ef7d30add1755c3bf138fdcb7
Matt Cowley <madcowley@gmail.com>
2013-06-01 11:21:13 -0400

    add uv_index client

---
+++ lib/epa/client/uv_index.rb
@@ -0,0 +1,58 @@
+# stolen from https://github.com/GSA-OCSIT/epa_uv_index
+require "epa_uv_index/version"
+require 'httparty'
+require 'json'
+
+module EpaUvIndex
+  class Client
+    class UvIndex
+      HOURLY_BASE_URL = "http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY"
+      DAILY_BASE_URL = "http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVDAILY"
+      
+      def self.hourly_for(params)
+        begin
+          do_request(HOURLY_BASE_URL, params)
+        rescue Exception => e
+          raise e
+        end
+      end
+    
+      def self.daily_for(params)
+        begin
+          do_request(DAILY_BASE_URL, params)
+        rescue Exception => e
+          raise e
+        end
+      end
+    
+      protected
+    
+      def self.do_request(base_url, params)
+        raise "Please provide either a city/state or a zip code." unless params[:zip] or (params[:city] and params[:state])
+        request_url = build_request_url(base_url, params)
+        http_response = HTTParty.get(request_url)
+        begin
+          evaluate_response(http_response)
+        rescue Exception => e
+          raise e
+        end      
+      end
+    
+      def self.build_request_url(base_url, params)
+        if params[:zip]
+          "#{base_url}/ZIP/#{params[:zip]}/json"
+        else
+          "#{base_url}/CITY/#{params[:city]}/STATE/#{params[:state]}/json"
+        end
+      end
+    
+      def self.evaluate_response(http_response)
+        if http_response.code == 200
+          return JSON.parse(http_response.body)
+        else
+          raise http_response.message
+        end
+      end
+    end
+  end
+end
